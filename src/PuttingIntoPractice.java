eb6f85e4311f09b2c1ba2fdd8bff7ced69175755
sasha <alex.idimeshev@gmail.com>
2023-12-15 18:21:49 +0300

    Second commit

--- src/PuttingIntoPractice.java
+++ src/PuttingIntoPractice.java
@@ -22,22 +22,23 @@ public class PuttingIntoPractice {
         );
         //1. Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшей
         //к большей)
+        System.out.println("1. Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшего к большей");
         transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
                 .sorted(Comparator.comparing(Transaction::getValue))
                 .forEach(System.out::println);
 
         System.out.println("==================================================");
-
         //2. Вывести список неповторяющихся городов, в которых работают трейдеры.
+        System.out.println("2. Вывести список неповторяющихся городов, в которых работают трейдеры");
         transactions.stream()
                 .map(transaction -> transaction.getTrader().getCity())
                 .distinct()
                 .forEach(System.out::println);
 
         System.out.println("==================================================");
-
         //3. Найти всех трейдеров из Кембриджа и отсортировать их по именам.
+        System.out.println("3. Найти всех трейдеров из Кембриджа и отсортировать их по именам");
         transactions.stream()
                 .map(Transaction::getTrader)
                 .distinct()
@@ -46,9 +47,9 @@ public class PuttingIntoPractice {
                 .forEach(System.out::println);
 
         System.out.println("==================================================");
-
         //4. Вернуть строку со всеми именами трейдеров, отсортированными в алфавитном
         //порядке.
+        System.out.println("4. Вернуть строку со всеми именами трейдеров, отсортированными в алфавитном порядке");
         List<Trader> list = transactions.stream()
                 .map(Transaction :: getTrader)
                 .distinct()
@@ -57,8 +58,8 @@ public class PuttingIntoPractice {
         System.out.println(list);
 
         System.out.println("==================================================");
-
         //5. Выяснить, существует ли хоть один трейдер из Милана.
+        System.out.println("5. Выяснить, существует ли хоть один трейдер из Милана");
         boolean anyTrader = transactions.stream()
                 .map(Transaction::getTrader)
                 .anyMatch(trader -> "Milan".equals(trader.getCity()));
@@ -69,14 +70,15 @@ public class PuttingIntoPractice {
         }
 
         System.out.println("==================================================");
-
-        //6 Вывести суммы всех транзакций трейдеров из Кембриджа.
+        //6. Вывести суммы всех транзакций трейдеров из Кембриджа.
+        System.out.println("6. Вывести суммы всех транзакций трейдеров из Кембриджа");
         transactions.stream()
                 .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
                 .forEach(tr -> System.out.println(tr.getValue()));
-        System.out.println("==================================================");
 
+        System.out.println("==================================================");
         //6* Вывести сумму всех транзакций трейдеров из Кембриджа.
+        System.out.println("6* Вывести сумму всех транзакций трейдеров из Кембриджа");
         int sumValue = transactions.stream()
                 .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
                 .mapToInt(Transaction::getValue)
@@ -84,21 +86,17 @@ public class PuttingIntoPractice {
         System.out.println(sumValue);
 
         System.out.println("==================================================");
-
         //7. Какова максимальная сумма среди всех транзакций?
-        int maxValue = transactions.stream()
-                .mapToInt(Transaction::getValue)
-                .max()
-                .orElse(0);
-        System.out.println(maxValue);
+        System.out.println("7. Какова максимальная сумма среди всех транзакций?");
+        transactions.stream()
+                .max(Comparator.comparingInt(Transaction::getValue))
+                .ifPresent(System.out::println);
 
         System.out.println("==================================================");
-
         //8. Найти транзакцию с минимальной суммой.
-        int minValue = transactions.stream()
-                .mapToInt(Transaction::getValue)
-                .min()
-                .orElse(0);
-        System.out.println(minValue);
+        System.out.println("8. Найти транзакцию с минимальной суммой");
+        transactions.stream()
+                .min(Comparator.comparingInt(Transaction::getValue))
+                .ifPresent(System.out::println);
     }
 }
